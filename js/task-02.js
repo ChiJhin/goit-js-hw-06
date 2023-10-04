c5160f916d8d72fe1be3d4a5327f8edc6025ac59
ChiJhin <jerek.d.lem@gmail.com>
2023-10-04 23:01:19 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const list = document.querySelector("#ingredients");
+
+const getAllIngradients = function (ingredients) {
+  return ingredients.map((ingredient) => {
+    const element = document.createElement("li");
+    element.classList.add(".item");
+    element.textContent = ingredient;
+    return element;
+  });
+};
+
+const allIngredients = getAllIngradients(ingredients);
+
+list.append(...allIngredients);
