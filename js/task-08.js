d734d37a293948f0dab9b637f17084dc9960ed8b
ChiJhin <jerek.d.lem@gmail.com>
2023-10-25 21:49:00 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const registrationForm = document.querySelector(".login-form");
 
+registrationForm.addEventListener("submit", verificationForm);
+
+function verificationForm(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("Усі поля повинні бути заповнені!");
+  } else {
+    console.log({
+      email: email,
+      password: password,
+    });
+    form.reset();
+  }
+}
