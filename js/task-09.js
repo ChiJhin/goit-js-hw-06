4b75564a688ea066e3b0850e3f7e498b8468adb7
ChiJhin <jerek.d.lem@gmail.com>
2023-10-25 23:20:15 +0300

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const body = document.querySelector("body");
+const showColor = document.querySelector(".color");
+const chengeColorBtn = document.querySelector(".change-color");
+
+function onChangeColor() {
+  const bgColor = getRandomHexColor();
+  body.style.backgroundColor = bgColor;
+  showColor.textContent = bgColor;
+}
+
+chengeColorBtn.addEventListener("click", onChangeColor);
